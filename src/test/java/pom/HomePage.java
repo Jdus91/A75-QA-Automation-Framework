2ad87853d7506bfa60304f416bd46f61a3de154c
Jdus91 <jennifer.de.jesus@testpro.io>
2025-10-10 15:08:13 -0400

    HW22
    committing changes as I progress on refactoring

--- src/test/java/pom/HomePage.java
+++ src/test/java/pom/HomePage.java
@@ -11,9 +11,62 @@ public class HomePage extends BasePage {
     }
 
     By userAvatarIcon = By.cssSelector("img.avatar");
+    By searchField = By.cssSelector("input[type='search']");
+    By viewAllButton = By.cssSelector("button[data-test='view-all-songs-btn']");
+    By addToButton = By.cssSelector("button[data-test='add-to-btn']");
+    private By notificationSuccess = By.cssSelector("div.success.show");
+
 
     public WebElement getUserAvatar() {
         return findElement(userAvatarIcon);
+    }
+
+    public void searchSong(String songName) {
+        WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(searchField));
+        searchInput.clear();
+        searchInput.sendKeys(songName);
+    }
+
+    public void clickViewAll() {
+        WebElement viewAll = wait.until(ExpectedConditions.elementToBeClickable(viewAllButton));
+        viewAll.click();
+    }
+
+    public void selectSongByTitle(String songTitle) {
+        WebElement song = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//td[@class='title' and text()='" + songTitle + "']")));
+        song.click();
+    }
+
+    public void clickAddToButton() {
+        WebElement addTo = wait.until(ExpectedConditions.elementToBeClickable(addToButton));
+        addTo.click();
+    }
+
+    public void choosePlaylist(String playlistName) {
+        WebElement playlist = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'" + playlistName + "')]")));
+        playlist.click();
+    }
+
+    public void clickPlay() {
+        WebElement playNextButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//i[@data-testid='play-next-btn']")));
+        WebElement playButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[@data-testid='play-btn']")));
+        playNextButton.click();
+        playButton.click();
+    }
+
+    public boolean isSongPlaying() {
+        WebElement soundBar = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[@data-testid='sound-bar-play']")));
+        return soundBar.isDisplayed();
+    }
+
+    public String getAddToPlaylistSuccessMsg() {
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(notificationSuccess));
+        return notification.getText();
     }
 
     public By playlistLocatorById() {
